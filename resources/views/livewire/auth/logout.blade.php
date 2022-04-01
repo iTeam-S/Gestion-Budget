6e9a2420d35293e316bbd55242470832f7e3216b
marcellinrabe <marcellinp20.aps1a@gmail.com>
2022-04-01 21:40:15 +0300

    suppression fichier inutile

--- resources/views/livewire/auth/logout.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <i class="fa fa-user me-sm-1 {{ in_array(request()->route()->getName(),['profile', 'my-profile']) ? 'text-white' : '' }}"></i>
-    <span class="d-sm-inline d-none {{ in_array(request()->route()->getName(),['profile', 'my-profile']) ? 'text-white' : '' }}" wire:click="logout">Se déconnecter</span>
-</div>
